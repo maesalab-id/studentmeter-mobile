fdf6086aa9d3c172fd93918df87ee5de07600b3e
EdgarJeremy <edgarpontoh3141@gmail.com>
2022-11-01 00:55:27 +0800

    (*) update UI

--- src/screens/panel/lecturer/Home.js
+++ src/screens/panel/lecturer/Home.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { View, ScrollView, Text, StyleSheet, TouchableNativeFeedback, Alert } from 'react-native';
-import { ListItem, Icon, Input, Avatar } from 'react-native-elements';
+import { View, ScrollView, Text, StyleSheet, TouchableNativeFeedback, Alert, Image } from 'react-native';
+import { ListItem, Icon } from 'react-native-elements';
 import moment from 'moment';
 import Loading from '../../Loading';
-import Border from '../../../components/Border';
 
 const style = StyleSheet.create({
     container: { flex: 1, backgroundColor: '#fff' },
@@ -14,6 +13,21 @@ const style = StyleSheet.create({
     classSubtitle: { color: '#fff' },
     classTitle: { color: '#fff', fontSize: 20, fontWeight: 'bold' }
 });
+
+function generateGreetings(time) {
+    var currentHour = time.format("HH");
+    if (currentHour >= 3 && currentHour < 12) {
+        return "pagi";
+    } else if (currentHour >= 12 && currentHour < 15) {
+        return "siang";
+    } else if (currentHour >= 15 && currentHour < 18) {
+        return "sore";
+    } else if (currentHour >= 18 || currentHour < 3) {
+        return "malam";
+    } else {
+        return "datang";
+    }
+}
 
 class Home extends React.Component {
     state = { ready: false, loading: false, newClass: '', schedules: [], time: new Date() };
@@ -43,28 +57,37 @@ class Home extends React.Component {
     }
     render() {
         const { navigation, user, logout } = this.props;
-        const { schedules, ready } = this.state;
+        const { schedules, ready, time } = this.state;
+        const greet = generateGreetings(moment(time));
         return (
             <ScrollView style={{ flex: 1, backgroundColor: '#fff' }}>
                 <View style={style.container}>
-                    <View style={{ marginTop: 15 }}>
-                        <Text style={{ textAlign: 'center' }}>Selamat pagi, {user.name.split(' ')[0]}!</Text>
+                    <View style={{ marginTop: 15, flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                        <Text style={{fontSize: 12}}>Selamat {greet}, {user.name.split(' ')[0]}! </Text>
+                        <Icon name={
+                            greet === 'pagi' || greet === 'siang' ? 'sunny' : (
+                                greet === 'sore' ? 'partly-sunny' : 'moon'
+                            )
+                        } type="ionicon" color={
+                            greet === 'pagi' || greet === 'siang' ? '#f1c40f' : (
+                                greet === 'sore' ? '#bdc3c7' : '#34495e'
+                            )
+                        } containerStyle={{ marginLeft: 5 }} size={17} />
                     </View>
-                    <View style={{ flex: 1, flexDirection: 'row', margin: 20, marginTop: 35 }}>
-                        <View style={{ flex: 2 }}>
-                            <Avatar
-                                rounded
-                                size="large"
+                    <View style={{ flex: 1, flexDirection: 'row', padding: 20 }}>
+                        <View style={{ flex: 2, justifyContent: 'center', alignItems: 'flex-start' }}>
+                            <Image
+                                style={{ width: '100%', height: 100, resizeMode: 'contain', borderRadius: 100 }}
                                 source={require('../../../images/user.png')}
                             />
                         </View>
-                        <View style={{ flex: 5, justifyContent: 'center' }}>
-                            <Text style={{ fontSize: 23 }}>{user.name}</Text>
+                        <View style={{ flex: 6, justifyContent: 'center', alignItems: 'flex-start', paddingLeft: 20 }}>
+                            <Text style={{ fontSize: 23, color: 'rgba(111, 202, 186, 1)', fontWeight: 'bold' }}>{user.name}</Text>
                             <Text style={{ fontSize: 14 }}>{user.major.name}</Text>
-                            <Text style={{ fontSize: 13 }}>{user.study_program.name}</Text>
+                            <Text style={{ fontSize: 12 }}>{user.study_program.name}</Text>
                         </View>
                         <View style={{ flex: 1, justifyContent: 'center' }}>
-                            <Icon name="logout" size={20} onPress={() => {
+                            <Icon name="logout" size={20} color="#e74c3c" onPress={() => {
                                 Alert.alert('Keluar akun?', 'Anda yakin ingin melakukan logout?', [
                                     { text: 'Keluar', onPress: logout },
                                     { text: 'Batal' }
@@ -76,24 +99,26 @@ class Home extends React.Component {
                         flex: 1,
                         flexDirection: 'row',
                         backgroundColor: 'rgba(111, 202, 186, .1)',
-                        // padding: 10, paddingBottom: 20, paddingTop: 20,
                         padding: 10,
-                        marginTop: 0,
-                        margin: 20,
+                        marginBottom: 20,
+                        // margin: 20,
+                        // marginLeft: 70,
+                        // marginRight: 70,
                         borderRadius: 5
                     }}>
-                        <View style={{ flex: 1 }}>
-                            <Text style={{ fontSize: 30, fontWeight: 'bold', color: 'rgba(111, 202, 186, 1)', textAlign: 'center' }}>{moment(this.state.time).format('hh:mm:ss A')}</Text>
+                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                            {/* <Icon name="time" type="ionicon" size={30} color="rgba(111, 202, 186, 1)" /> */}
+                            <Text style={{ fontSize: 30, color: 'rgba(111, 202, 186, 1)', fontWeight: 'bold' }}> {moment(this.state.time).format('hh:mm:ss A')}</Text>
                         </View>
                     </View>
                     <View>
-                        <Text style={{ marginLeft: 16, fontSize: 17, fontWeight: 'bold' }}>Kelas Mengajar</Text>
+                        <Text style={{ marginLeft: 16, fontSize: 17, fontWeight: 'bold', color: 'rgba(111, 202, 186, 1)' }}>Kelas Mengajar</Text>
                         {ready ? (
                             schedules.length ? (
                                 schedules.map((s, i) => (
                                     <ListItem Component={TouchableNativeFeedback} onPress={() => navigation.navigate('Room', { schedule: s, refreshHome: this.fetch.bind(this) })} key={i} bottomDivider>
                                         <ListItem.Content>
-                                            <ListItem.Title style={{ fontSize: 15 }}>{s.subject.name}</ListItem.Title>
+                                            <ListItem.Title style={{ fontSize: 15, fontWeight: 'bold' }}>{s.subject.name}</ListItem.Title>
                                             <ListItem.Subtitle style={{ fontSize: 13 }}>{s.class.name} - {this.getTotals(s)}</ListItem.Subtitle>
                                         </ListItem.Content>
                                         <ListItem.Chevron />
